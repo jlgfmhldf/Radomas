e2239980f0a73db79271f57fa69ea2f2b79c071f
Земцева Виктория <jlgfmhldf@gmail.com>
2017-10-26 13:44:18 +0300

    feat(faq, tabs)

---
+++ app/blocks/tabs/index.js
@@ -0,0 +1,19 @@
+import jTabs from 'jtabs'
+
+const tabsContainers = document.querySelector('.js-tabs')
+const buttons = document.querySelectorAll('.js-tabs-button')
+const tabs = document.querySelectorAll('.js-tabs-tab')
+
+jTabs({
+	container: tabsContainers,
+	buttons,
+	tabs,
+	activeBtnClass: 'tabs__button_active',
+	activeTabClass: 'tabs__tab_active',
+})
+
+
+
+// tabsContainers.forEach(elem => jTabs({
+// 	contain
+// }))
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,4 @@
 import '../blocks/main-slider'
 import '../blocks/blue-slider'
 import '../blocks/service-folding-info'
+import '../blocks/tabs'
