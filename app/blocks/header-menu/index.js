726bc63f71fecc1d2b6fde6cc48936c427da8330
Земцева Виктория <jlgfmhldf@gmail.com>
2017-12-03 16:47:47 +0300

    feat(menu): mobile

--- app/blocks/header-menu/index.js
+++ app/blocks/header-menu/index.js
@@ -2,9 +2,25 @@ import navigation from 'formstone/dist/js/navigation'
 import $ from 'jquery'
 
 
-$('.js-menu').navigation({
+const $menu = $('.js-menu')
+const $submenuBtn = $('.js-submenu-btn')
+const listActiveClass = 'header-menu__submenu_show'
+const listSelector = '.js-submenu-list'
+
+$menu.navigation({
 	type: 'overlay',
 	gravity: 'left',
 	maxWidth: '1040px',
 	label: false,
+})
+
+$submenuBtn.on('click', function () {
+	event.preventDefault()
+
+	const $btn = $(this)
+	const $submenuList = $btn.next(listSelector)
+	const $internalLists = $submenuList.find(listSelector)
+
+	$submenuList.toggleClass(listActiveClass)
+	$internalLists.removeClass(listActiveClass)
 })
